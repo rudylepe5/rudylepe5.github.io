c8b2a8be78fdd2dc5be1660be76b04aa0aa5a23f
rudylepe5 <rudylepe5@Rudys-MBP.lan>
2015-03-08 19:08:32 -0600

    Prueba

---
+++ js/pagina1.js
@@ -0,0 +1,14 @@
+/* I did all my css in here*/
+$(".par").css("font-size","50px").css("color", "blue");
+$(".head").css("color","green").css("font-size","200px").css("text-align","center");
+$(".yo").css("color","red").css("font-size","30px");
+$(".pregunta").css("color","purple").css("font-size","100px").css("text-align","center");
+$("#solo").css("color","purple").css("font-size","100px").css("text-align","center");
+
+$("#solo").click(function(){
+	$("#mas").text("Describe why you agree or not with the new boarding pass");
+	$("#mas").css("color","pink").css("font-size","50px");
+});
+$("#yolo").dblclick(function(){
+	$(".swag").text("Holiiiiiiiiiiiis");
+});
