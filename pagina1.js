2599a40a2d1fd85c43bb7967d449d3993ec9d816
rudylepe5 <rudylepe5@Rudys-MacBook-Pro.local>
2015-04-27 15:13:28 -0500

    Prueba 18

--- pagina1.js
+++ pagina1.js
@@ -6,17 +6,4 @@ $(".imagen").hover(
 		$(".cool").css("visibility","hidden");
 	}
 );
-$(document).ready(function(){ 
-
-	var n = $(document).width();
-
-	if (n > 500) {
-		$("#video").css("width","970px").css("height","600px");
-		$(body).css("background-color","pink");
-	} 
-	else {
-		$("#video").css("width","100px").css("height","100px");
-	}
-
-});
 
